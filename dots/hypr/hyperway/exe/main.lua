f9ae6ca50ae0410352410dec5c486919fcffc633
0xarch <alanqa-ops@outlook.com>
2026-06-05 11:23:21 +0800

    add wallpaper at startup

--- dots/hypr/hyperway/exe/main.lua
+++ dots/hypr/hyperway/exe/main.lua
@@ -1,16 +1,19 @@
-CONFIG_DIR = "~/.config/hypr/hyperway/exe/"
+CONFIG_DIR = "~/.config/hypr/hyperway"
+
+EXE_DIR = CONFIG_DIR .. "/exe"
 
 if os.getenv("HYPERWAY_APPEARANCE") then
-	CONFIG_DIR = "~/.config/hypr/hyperway/appearance/" .. os.getenv("HYPERWAY_APPEARANCE") .. "/"
+	EXE_DIR = CONFIG_DIR .. "/appearance/" .. os.getenv("HYPERWAY_APPEARANCE")
 end
 
 if os.getenv("HYPERWAY_EXE_DEFAULT") == "1" then
 	hl.on("hyprland.start", function()
-		hl.exec_cmd("mako -c " .. CONFIG_DIR .. "mako/config")
-		hl.exec_cmd("fcitx5 -d")
-		hl.exec_cmd(CONFIG_DIR .. "waybar/start.sh")
+		hl.exec_cmd("mako -c " .. CONFIG_DIR .. "/mako/config")
+		hl.exec_cmd(CONFIG_DIR .. "/waybar/start.sh")
 		hl.exec_cmd(
-			"swayosd-server --config " .. CONFIG_DIR .. "swayosd/config.toml -s " .. CONFIG_DIR .. "swayosd/style.css"
+			"swayosd-server --config " .. CONFIG_DIR .. "/swayosd/config.toml -s " .. CONFIG_DIR .. "/swayosd/style.css"
 		)
+		hl.exec_cmd("fcitx5 -d")
+		hl.exec_cmd(CONFIG_DIR .. "/util/darkmode/wallpaper.sh")
 	end)
 end
