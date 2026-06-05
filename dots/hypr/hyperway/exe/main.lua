3cb2c82a77efd84c86a1c43b0704c355de903772
0xarch <alanqa-ops@outlook.com>
2026-06-05 11:47:20 +0800

    exe: fix execution

--- dots/hypr/hyperway/exe/main.lua
+++ dots/hypr/hyperway/exe/main.lua
@@ -8,10 +8,10 @@ end
 
 if os.getenv("HYPERWAY_EXE_DEFAULT") == "1" then
 	hl.on("hyprland.start", function()
-		hl.exec_cmd("mako -c " .. CONFIG_DIR .. "/mako/config")
-		hl.exec_cmd(CONFIG_DIR .. "/waybar/start.sh")
+		hl.exec_cmd("mako -c " .. EXE_DIR .. "/mako/config")
+		hl.exec_cmd(EXE_DIR .. "/waybar/start.sh")
 		hl.exec_cmd(
-			"swayosd-server --config " .. CONFIG_DIR .. "/swayosd/config.toml -s " .. CONFIG_DIR .. "/swayosd/style.css"
+			"swayosd-server --config " .. EXE_DIR .. "/swayosd/config.toml -s " .. EXE_DIR .. "/swayosd/style.css"
 		)
 		hl.exec_cmd("fcitx5 -d")
 		hl.exec_cmd(CONFIG_DIR .. "/util/darkmode/wallpaper.sh")
