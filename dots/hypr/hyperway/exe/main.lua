5152553a6b96eeb7e344523b320fbc81ae63fe32
0xarch <alanqa-ops@outlook.com>
2026-06-07 19:12:34 +0800

    exe: add kded6 as optional

--- dots/hypr/hyperway/exe/main.lua
+++ dots/hypr/hyperway/exe/main.lua
@@ -15,6 +15,7 @@ if os.getenv("HYPERWAY_EXE_DEFAULT") == "1" then
 		)
 		hl.exec_cmd("fcitx5 -d")
 		hl.exec_cmd("/usr/lib/polkit-kde-authentication-agent-1")
+		hl.exec_cmd("kded6")
 		hl.exec_cmd(CONFIG_DIR .. "/util/darkmode/wallpaper.sh")
 	end)
 end
