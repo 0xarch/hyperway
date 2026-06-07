f9695d5697905c3f234a53a3881a35a640d0e2d3
0xarch <alanqa-ops@outlook.com>
2026-06-07 13:16:24 +0800

    update waybar; freeze screen when capture

--- dots/hypr/hyperway/keybind/basicapp.lua
+++ dots/hypr/hyperway/keybind/basicapp.lua
@@ -1,8 +1,8 @@
 if os.getenv("HYPERWAY_KEYBIND_BASICAPP") == "1" then
 	hl.bind("SUPER + RETURN", hl.dsp.exec_cmd("kitty"))
 	hl.bind("SUPER + SUPER_L", hl.dsp.exec_cmd("pkill fuzzel || fuzzel"))
-	hl.bind("SUPER + SHIFT + S", hl.dsp.exec_cmd("hyprshot -m region"))
+	hl.bind("SUPER + SHIFT + S", hl.dsp.exec_cmd("hyprshot -z -m region"))
 	hl.bind("SUPER + SHIFT + C", hl.dsp.exec_cmd("hyprpicker -a"))
 	hl.bind("SUPER + SHIFT + E", hl.dsp.exec_cmd("wlogout"))
-	hl.bind("Print", hl.dsp.exec_cmd("hyprshot -m output -m active"))
+	hl.bind("Print", hl.dsp.exec_cmd("hyprshot -z -m output -m active"))
 end
