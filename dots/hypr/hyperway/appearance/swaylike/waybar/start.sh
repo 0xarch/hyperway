934fe96db925b0f9628050636b22abe8132fc3d9
0xarch <alanqa-ops@outlook.com>
2026-06-16 16:58:33 +0800

    new workspace widget (simulated)

--- dots/hypr/hyperway/appearance/swaylike/waybar/start.sh
+++ dots/hypr/hyperway/appearance/swaylike/waybar/start.sh
@@ -10,9 +10,13 @@ if [ "$COUNT" -eq ""]; then
 fi
 
 persistent_json=""
+workspace_list=""
+workspace_conf=""
 # persistent_json="{"
 for i in $(seq 1 $COUNT); do
   persistent_json+="\"$i\": true, "
+  workspace_list+="\"custom/workspace#$i\","
+  workspace_conf+="\"custom/workspace#$i\": { \"on-scroll-down\": \"hyprctl dispatch 'hl.dsp.focus({workspace=\\\\\\\"+1\\\\\\\"})'\", \"on-scroll-up\": \"hyprctl dispatch 'hl.dsp.focus({workspace=-1})'\", \"on-click\": \"hyprctl dispatch 'hl.dsp.focus({workspace=$i})'\", \"exec\": \"\$HOME/.config/hypr/hyperway/util/workspace/waybar.sh $i\", \"signal\": 29},"
 done
 persistent_json=${persistent_json%, }
 # persistent_json+="}"
@@ -22,7 +26,9 @@ mkdir -p $(dirname $PARSED_CONFIG)
 cp $CONFIG_DIR/config.jsonc $PARSED_CONFIG
 
 sed -i "s|%%PERSISTENT%%|$persistent_json|" $PARSED_CONFIG
+sed -i "s|%%WORKSPACE_LIST%%|$workspace_list|" $PARSED_CONFIG
+sed -i "s|%%WORKSPACE%%|$workspace_conf|" $PARSED_CONFIG
 
 waybar -c "$PARSED_CONFIG" -s "$CONFIG_DIR/style.css"
 
-rm $PARSED_CONFIG
+# rm $PARSED_CONFIG
